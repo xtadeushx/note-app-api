b285456055cfcb14b9e9d55c2082c6fc58898f63
xtadeushx <alekscherkasov13@gmail.com>
2023-08-04 16:11:15 +0300

    fix: change message for CreateNoteDto

--- src/notes/dto/create-note.dto.ts
+++ src/notes/dto/create-note.dto.ts
@@ -1,7 +1,10 @@
 import { IsString, Length, MinLength, MaxLength } from 'class-validator';
 
 export class CreateNoteDto {
-  @MinLength(5)
+  @MinLength(5, {
+    message:
+      'Title is too short. Minimal length is $constraint1 characters, but actual is $value',
+  })
   @MaxLength(20)
   @IsString()
   title: string;
